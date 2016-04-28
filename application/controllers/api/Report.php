1dae5a3e5668c0e44436c20f96df38d4ab8404ff
Ainul Yaqin <ainul.top@gmail.com>
2016-04-28 14:07:44 +0700

    igs

--- application/controllers/api/Report.php
+++ application/controllers/api/Report.php
@@ -20,6 +20,17 @@ class Report extends REST_Controller
         $cpa = $this->db->where('CID', $this->get("id"))->from('42cpa')->get();
         $csh = $this->db->where('CID', $this->get("id"))->from('33csh')->get();
         $cmb = $this->db->where('CID', $this->get("id"))->from('35cmb')->get();
+        $cpc = $this->db->where('CID', $this->get("id"))->from('39cpc')->get();
+        //bank
+        //SELECT * FROM 41cbk JOIN 97bkl ON 41cbk.`BankID` = 97bkl.`ID`
+        $cbk = $this->db->where('41cbk.CID', $this->get("id"))->join("97bkl","41cbk.BankID = 97bkl.ID")->from('41cbk')->get();
+
+        $cst = $this->db->where('CID', $this->get("id"))->where("Until","December")->limit(1)->order_by("Year","DESC")->from('40cst')->get();
+        $cstVal = "";
+        if($cst->num_rows==1){
+            $cstRow = $cst->row_array();
+            $cstVal = $cstRow['Amount'];
+        }
 
         if ($cmp->num_rows() == 1) {
             $cbrLists = array();
@@ -62,6 +73,19 @@ class Report extends REST_Controller
                     "WorkExperience" => "",
                     "Age" => ""
                 );
+            }
+
+            $cbkLists = array();
+            foreach ($cbk->result_array() as $cbkList) {
+                $cbkLists[]["BankInfo"] = array(
+                    "Name" => $cbkList['Name'],
+                    "Code" => ""
+                );
+            }
+
+            $cpcLists = array();
+            foreach ($cpc->result_array() as $cpcList) {
+                $cpcLists[] = $cpcList['name'];
             }
 
             $cmpRow = $cmp->row_array();
@@ -114,7 +138,7 @@ class Report extends REST_Controller
                         "PaidUpCapital" => $cmpRow['PaidCap'],
                         "PaidUpMoneyId" => "IDR",
                         "PaidUpCapitalUSD" => "",
-                        "OperationStatus" => 1,
+                        "OperationStatus" => 1, //butuh penyesuaian
                         "ParentId" => "",
                         "Parent" => "",
                         "UParentId" => "",
@@ -139,7 +163,99 @@ class Report extends REST_Controller
                         "Managers" => $cmbLists,
                         "BranchDetailInfos" => $cbrLists,
                         "ShareHolders" => $cshLists
-                    )
+                    ),
+                    "Operation" => array(
+                        "BusinessType" => "",
+                        "Products" => implode(",",$cpcLists),
+                        "Brands" => "",
+                        "BrandType" => "",
+                        "SICLocal" => "",
+                        "CodeStandard" => "",
+                        "TrendOfIndustry" => "",
+                        "ProductionCapacity" => "",
+                        "PremiseSize" => "",
+                        "ChinaSIC" => "",
+                        "PremiseValue" => "",
+                        "Location" => "",
+                        "SizeUnit" => "",
+                        "HoldingsType" => "",
+                        "PremiseValueUSD" => "",
+                        "PremiseMoneyID" => "",
+                        "MainSalesTerritory" => "",
+                        "TotalSales" => $cstVal,
+                        "TotalMoneyID" => ($cstVal!=""?"IDR":""),
+                        "TotalSalesUSD" => "",
+                        "ExportMode" => "",
+                        "DomesticSaleMode" => "",
+                        "TotalExport" => "",
+                        "ExportMoneyID" => "",
+                        "TotalExportUSD" => "",
+                        "PurchasingArea" => "",
+                        "TotalPurchase" => "",
+                        "TotalPurchaseMoneyId" => "",
+                        "TotalPurchaseUSD" => "",
+                        "ImportAmount" => "",
+                        "ImportMoneyID" => "",
+                        "ImportUSD" => "",
+                        "DomesticPurchaseRatio" => "",
+                        "ForeignPurchaseRatio" => "",
+                        "NAICS" => "",
+                        "ImportMode" => "",
+                        "DomesticPurchaseMode" => ""
+                    ),
+                    "CreditRecord" => array(
+                        "RiskScore" => "",
+                        "RiskAnalysis" => "",
+                        "CreditRecommendation" => "",
+                        "BankCreditMax" => "",
+                        "BankCreditMoneyID" => "",
+                        "BankCreditMaxUSD" => "",
+                        "BankComment" => "",
+                        "PaymentScore" => "",
+                        "NoOfPaymentRecords" => "",
+                        "TotalAmount" => "",
+                        "TotalAmountMoneyID" => "",
+                        "TotalAmountUSD" => "",
+                        "HighestPayment" => "",
+                        "HighestMoneyID" => "",
+                        "HighestUSD" => "",
+                        "HighestCredt" => "",
+                        "HighestCreditMoneyID" => "",
+                        "HighestCreditUSD" => "",
+                        "AverageDBT" => "",
+                        "BadPaymentFlag" => "",
+                        "TradeHistory" => "",
+                        "MonthlyAverage" => "",
+                        "MonthlyAverageMoneyID" => "",
+                        "MonthlyAverageUSD" => "",
+                        "MortgageAmount" => "",
+                        "MortgageMoneyID" => "",
+                        "MortgageAmountUSD" => "",
+                        "GuarantorAmount" => "",
+                        "GuarantorAmountMoneyID" => "",
+                        "GuarantorAmountUSD" => "",
+                        "GuaranteeAmount" => "",
+                        "GuaranteeAmountMoneyID" => "",
+                        "GuaranteeAmountUSD" => "",
+                        "EDFValue" => "",
+                        "EDFTrend" => "",
+                        "EDFDate" => "",
+                        "Justice" => "",
+                        "SoverguaranteeFlag" => "",
+                        "BankInfos" => $cbkLists,
+                        "MortgageDetails" => ""
+                    ),
+                    "Public" => array(
+                        "BulletinFlg" => "",
+                        "NewsFlag" => "",
+                        "LitigationFlag" => "",
+                        "BankruptcyFlg" => "",
+                        "MortgateFlag" => "",
+                        "OtherPublicRecordFlag" => "",
+                        "LienFlag" => "",
+                        "Note" => ""
+                    ),
+                    "Financials" => ""
                 )
             ));
         }
